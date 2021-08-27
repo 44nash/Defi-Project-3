e572c17946b39b74a700831a345f244b2381869c
44nash <44mcode@gmail.com>
2021-08-26 22:43:35 -0400

    buttons for saw liq and farming up

--- src/components/DeFi/DeFi.js
+++ src/components/DeFi/DeFi.js
@@ -11,6 +11,12 @@ import { ethers } from 'ethers';
 
 import Web3 from 'web3';
 import factoryABI from './factoryABI';
+import Modal from 'react-bootstrap/Modal';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { Form, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
+
+// https://react-icons.github.io/react-icons
+import { ImArrowRight } from 'react-icons/im';
 
 // https://shmoji.medium.com/web3-react-connect-users-to-metamask-or-any-wallet-from-your-frontend-241fd538ed39
 export default function DeFi(props) {
@@ -21,6 +27,33 @@ export default function DeFi(props) {
   // const fm = new Fortmatic('pk_live_7450BD27E0F9931C', 'rinkeby');
   // const fm = new Fortmatic('pk_live_7450BD27E0F9931C', 'ropsten');
   // const fm = new Fortmatic('pk_live_7450BD27E0F9931C', 'kovan');
+
+  const [allState, setAllstate] = useState({
+    swap: false,
+    liquidity: false,
+    yieldFarm: false,
+  });
+
+  const handleSwap = () => {
+    setAllstate({ swap: !allState.swap, liquidity: false, yieldFarm: false });
+  };
+
+  const handleLiquidity = () => {
+    setAllstate({ swap: true, liquidity: true, yieldFarm: false });
+  };
+
+  const handleYieldFarm = () => {
+    setAllstate({ swap: true, liquidity: true, yieldFarm: true });
+  };
+
+  ///===========
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  ///===========
 
   const [balance, setBalance] = useState(0);
 
@@ -38,7 +71,7 @@ export default function DeFi(props) {
 
     var accountInterval = setInterval(function () {}, 100);
 
-    //factoryContract.events.Transfer()
+    // factoryContract.events.Transfer()
 
     const exchangeAddress = factoryContract.methods.getExchange('USDC');
     console.log(exchangeAddress);
@@ -117,7 +150,103 @@ export default function DeFi(props) {
           Connect to MetaMask
         </Button>
         <br />
+
+        {/*  Picking: Swap Liquid  or Farm */}
+        <div>
+          <br />
+          {/* checked={!!allState.swap} */}
+          <Button
+            className="arrow-button"
+            variant={allState.swap ? 'success' : 'info'}
+            onClick={handleSwap}
+          >
+            <ImArrowRight className="rightArrow" />
+            <span> Swap </span>
+          </Button>
+          <br />
+          <br />
+
+          <Button
+            variant={allState.liquidity ? 'success' : 'info'}
+            onClick={handleLiquidity}
+          >
+            <ImArrowRight className="rightArrow" />
+            <span> Add liquidity only, (no farming) </span>
+          </Button>
+          <br />
+          <br />
+          <Button
+            variant={allState.yieldFarm ? 'success' : 'info'}
+            onClick={handleYieldFarm}
+          >
+            <ImArrowRight className="rightArrow" />
+            <span> Yield Farm PRJT </span>
+          </Button>
+          <br />
+          <br />
+          <Button variant="info" onClick={handleShow}>
+            SUBMIT
+          </Button>
+
+          <Modal
+            show={show}
+            onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>Modal title</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              Are these the one's you choose ?
+              <InputGroup>
+                <InputGroup.Radio
+                  value={allState.swap}
+                  aria-label="Radio button for following text input"
+                  checked={!!allState.swap}
+                  name="swap"
+                />
+                <label for="swap" style={{ paddingLeft: '10px' }}>
+                  {' '}
+                  Swap{' '}
+                </label>
+                <br></br>
+              </InputGroup>
+              <InputGroup>
+                <InputGroup.Radio
+                  value={allState.liquidity}
+                  aria-label="Radio button for following text input"
+                  checked={!!allState.liquidity}
+                />
+                <label for="swap" style={{ paddingLeft: '10px' }}>
+                  Liquidity
+                </label>
+              </InputGroup>
+              <InputGroup>
+                <InputGroup.Radio
+                  value={allState.yieldFarm}
+                  aria-label="Radio button for following text input"
+                  checked={!!allState.yieldFarm}
+                />
+                <label for="swap" style={{ paddingLeft: '10px' }}>
+                  Yield-Farm
+                </label>
+              </InputGroup>
+              <p> {JSON.stringify(allState)} </p>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button variant="primary">Final Submit </Button>
+            </Modal.Footer>
+          </Modal>
+          <br />
+        </div>
+
         <br />
+
+        <p> {JSON.stringify(allState)} </p>
 
         <Button
           variant="danger"
@@ -127,6 +256,7 @@ export default function DeFi(props) {
           Disconnect
         </Button>
         <br />
+
         <br />
         {active ? (
           <span>
@@ -139,6 +269,9 @@ export default function DeFi(props) {
         ) : (
           <span>Not connected</span>
         )}
+
+        {/* <ImArrowRight />
+        <ImArrowRight /> */}
       </div>
     </div>
   );
